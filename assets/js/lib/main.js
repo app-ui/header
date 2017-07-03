fc354bdeb697fc5963c823100c35c1b0e2ae8d68
Makis Tracend <makis.tracend@gmail.com>
2017-07-02 19:33:30 -0700

    main logic

---
+++ assets/js/lib/main.js
@@ -0,0 +1,53 @@
+/*
+ * APP UI: Header
+ * Source: https://github.com/app-ui/header
+ * Copyright © Makesites.org
+ *
+ * Initiated by Makis Tracend (@tracend)
+ * Distributed through [Makesites.org](http://makesites.org)
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ */
+
+(function (lib) {
+
+	//"use strict";
+
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		var deps = ['jquery', 'underscore']; // condition when app is part of the array?
+		define('app.ui.header', deps, lib);
+	} else if ( typeof module === "object" && module && typeof module.exports === "object" ){
+		// Expose as module.exports in loaders that implement CommonJS module pattern.
+		module.exports = lib;
+	} else {
+		// Browser globals
+		var Query = window.jQuery || window.Zepto || window.vQuery;
+		lib( Query, window._ );
+	}
+}(function ( $, _ ) {
+
+	APP = APP || window.APP || null;
+	var isAPP = ( APP !== null );
+	// support for APP() view if available...
+	var View = ( isAPP && typeof APP.View !== "undefined" ) ? APP.View : Object;
+
+
+{{{lib}}}
+
+	if( isAPP ){
+		APP.UI = APP.UI || {};
+		APP.UI.Header = Header;
+	}
+
+	// If there is a window object, that at least has a document property
+	if( typeof window === "object" && typeof window.document === "object" ){
+		// update APP namespace
+		if( isAPP ){
+			window.APP = APP;
+		}
+	}
+
+	// for module loaders:
+	return Header;
+
+}));
